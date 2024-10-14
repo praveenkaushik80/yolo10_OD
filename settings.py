e2a89c39ea8efe4d3b5b0780cd607ec816ebf535
Praveen Kaushik <praveenkaushik80@icloud.com>
2024-10-14 20:42:35 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -36,8 +36,8 @@ VIDEOS_DICT = {
 def attempt_download_from_hub(repo_id, hf_token=None):
     # https://github.com/fcakyon/yolov5-pip/blob/main/yolov5/utils/downloads.py
     from huggingface_hub import hf_hub_download, list_repo_files
-    from huggingface_hub.utils._errors import RepositoryNotFoundError
-    from huggingface_hub.utils._validators import HFValidationError
+    #from huggingface_hub.utils._errors import RepositoryNotFoundError
+    #from huggingface_hub.utils._validators import HFValidationError
     try:
         repo_files = list_repo_files(repo_id=repo_id, repo_type='model', token=hf_token)
         model_file = [f for f in repo_files if f.endswith('.pt')][0]
@@ -48,7 +48,8 @@ def attempt_download_from_hub(repo_id, hf_token=None):
             token=hf_token,
         )
         return file
-    except (RepositoryNotFoundError, HFValidationError):
+    except:
+    # except (RepositoryNotFoundError, HFValidationError):
         return None
 
 # ML Model config
